07cfe27ca9f17d6bed06a4361ae98f455a3642aa
PYQ0423 <403995084@qq.com>
2019-11-14 10:13:03 +0800

    @PathVariable, the rest API.

--- src/main/java/com/example/demo/dao/AlienRepo.java
+++ src/main/java/com/example/demo/dao/AlienRepo.java
@@ -1,12 +1,11 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Alien;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface AlienRepo extends CrudRepository<Alien, Integer> {
-    List<Alien> findByTech(String tech);
 
-    List<Alien> findByAidGreaterThan(int aid);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into alien values (101,'pyq','JAVA');
 insert into alien values (102,'jqy','Android');
-insert into alien values (103,'plh','Python');
+insert into alien values (103,'plh','JAVA');
 insert into alien values (104,'gy','JavaScript');
 insert into alien values (105,'lqy','Scala');
